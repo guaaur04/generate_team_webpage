920beb9f8fc1ca30661695d266f6b18cb6b928c2
V Guardado <auricia.guardado@gmail.com>
2021-03-02 00:35:36 -0800

    edit prompt in manager, success render cards

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
-const Employee = require("./lib/Employee");
+// const Employee = require("./lib/Employee");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
@@ -11,124 +11,146 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const finalArray = [];
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
 function generateTeam() {
 
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "Are you finished adding employees?",
+                name: "out",
+                choices: ["Yes", "No"],
+            },
+        ])
+        .then((confirm) => {
+            if (confirm.out === "No") {
 
-    
-    inquirer.prompt([
-        {
-            type: "list",
-            message: "What's your role on this team?",
-            name: "role",
-            choices: ["Engineer, Intern, Manager"],
-            default: "Engineer, Intern, Manager",
-        },
-
-        {
-            type: "input",
-            message: "Enter name:",
-            name: "name",
-            default: "Anonymous",
-        },
-
-
-        {
-            type: "input",
-            message: "Enter email address:",
-            name: "email",
-            default: "placeholder@autoflower.com",
-        },
-
-        {
-            type: "input",
-            message: "Employee ID:",
-            name: "id",
-            default: "1,2,3,4,5,6",
-        },
-
-    ])
-        
-        .then((data) => {
-            if((data.role === "Manager") {
-                inquirer.prompt ([
-                    {
-                        type: "input",
-                        message: "Enter office number:",
-                        name: "officeNumber",
-                        default: "#",
-                    },
-                ])
-                .then((manage) => {
-                    const manager = new Manager(
-                      data.name,
-                      data.id,
-                      data.email,
-                      manage.officeNumber
-                    );
-
-                if (
-                        finalArray.find((element) => element.role === "Manager")
-                      ) {
-                        console.log("Hello!");
-                        generateTeam();
-                      } else {
-                        finalArray.push(manager);
-                        generateTeam();
-                      }
-                    });  
-            } else if (data.role ==="Engineer") {
-                inquirer.promp ([
-                    {
-                        type: "input",
-                        message: "Enter Github username:",
-                        name: "github",
-                        default: "Github Username",  
-                    },
-                ])
-                .then((engine) => {
-                    const engineer = new Engineer(
-                      data.name,
-                      data.id,
-                      data.email,
-                      engine.github
-                    );
-                    finalArray.push(engineer);
-                    generateTeam();
-                  });
-            }  else if (data.role ==="Intern") {
                 inquirer.prompt([
                     {
-                        type: "input",
-                        message: "Enter school:",
-                        name: "school",
-                        default: "The Evergreen State College",    
+                        type: "list",
+                        message: "What's your role on this team?",
+                        name: "role",
+                        choices: ["Engineer", "Intern", "Manager"],
                     },
+
+                    {
+                        type: "input",
+                        message: "Enter name:",
+                        name: "name",
+                        default: "Anonymous",
+                    },
+
+
+                    {
+                        type: "input",
+                        message: "Enter email address:",
+                        name: "email",
+                        default: "placeholder@autoflower.com",
+                    },
+
+                    {
+                        type: "input",
+                        message: "Employee ID:",
+                        name: "id",
+                        default: "#",
+                    },
+
                 ])
-                .then((student) => {
-                    const intern = new Intern(
-                      data.name,
-                      data.id,
-                      data.email,
-                      student.school
-                    );
-                    finalArray.push(intern);
-                    generateTeam();
-                  });
-            }  
 
+                    .then((data) => {
+                        if (data.role === "Manager") {
+                            inquirer.prompt([
+                                {
+                                    type: "input",
+                                    message: "Enter office number:",
+                                    name: "officeNumber",
+                                    default: "#",
+                                },
+                            ])
+                                .then((manage) => {
+                                    const manager = new Manager(
+                                        data.name,
+                                        data.id,
+                                        data.email,
+                                        manage.officeNumber
+                                    );
+
+                                    if (
+                                        finalArray.find((element) => element.role === "Manager")
+                                    ) {
+                                        console.log("Hello!");
+                                        generateTeam();
+                                    } else {
+                                        finalArray.push(manager);
+                                        generateTeam();
+                                    }
+                                });
+                        } else if (data.role === "Engineer") {
+                            inquirer.promp([
+                                {
+                                    type: "input",
+                                    message: "Enter Github username:",
+                                    name: "github",
+                                    default: "Github Username",
+                                },
+                            ])
+                                .then((engine) => {
+                                    const engineer = new Engineer(
+                                        data.name,
+                                        data.id,
+                                        data.email,
+                                        engine.github
+                                    );
+                                    finalArray.push(engineer);
+                                    generateTeam();
+                                });
+                        } else if (data.role === "Intern") {
+                            inquirer.prompt([
+                                {
+                                    type: "input",
+                                    message: "Enter school:",
+                                    name: "school",
+                                    default: "The Evergreen State College",
+                                },
+                            ])
+                                .then((student) => {
+                                    const intern = new Intern(
+                                        data.name,
+                                        data.id,
+                                        data.email,
+                                        student.school
+                                    );
+                                    finalArray.push(intern);
+                                    generateTeam();
+                                });
+                            }
+
+                        });
+                    
+
+            } else {
+            produceHTML(render(finalArray)); 
+            }
         });
-    } else {
-        produceHTML(render(finalArray));
     }
-});
-}
 
-ge
+    generateTeam();
 
-           
+    const produceHTML = (savingString) => {
+    fs.writeFile("./output/team.html", savingString, (err) => {
+        if(err) throw err;
+        console.log('Success!');
+    });
+
+};
+
+
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
